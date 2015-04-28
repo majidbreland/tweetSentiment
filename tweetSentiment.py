f3158c3fa8efbe36993283302d132a65a73c31ad
majidbreland <majid.breland@gmail.com>
2015-04-28 11:43:56 -0500

    added sentiment of trending and diff output options

--- tweetSentiment.py
+++ tweetSentiment.py
@@ -1,39 +1,71 @@
 from pattern.web import Twitter, plaintext
 from pattern.web.locale import geocode
 
+import csv
 import indicoio
 
+
+#enter your api key here
 indicoio.config.api_key = ""
 
+
 class tweetSentiment(object):
+
 
 	def __init__(self, topic, tweetCount):
 		self.topic = topic
 		self.tweetCount = tweetCount
+		self.t = Twitter(language='EN')
+		self.i = None
 
-	def calculate(self):
-		t = Twitter(language='EN')
-		i = None
+	def fArray(self):
+		'''full array including tweet and sentiment'''	
+		fullArray = []
+
+		for tweet in self.t.search(self.topic, start=self.i, count = self.tweetCount):
+			fullArray.append([tweet.text,indicoio.sentiment(tweet.text)])
+			self.i = tweet.id
+
+		return fullArray
+
+	def sArray(self):
+		'''calculate sentiment '''
 		sentimentArray = []
 
-		for tweet in t.search(self.topic, start=i, count = self.tweetCount):
+		for tweet in self.t.search(self.topic, start=self.i, count = self.tweetCount):
 			sentimentArray.append(indicoio.sentiment(tweet.text))
-			i = tweet.id
+			self.i = tweet.id
 
 		return sentimentArray
 
-	def average(self,sentimentArray):
-		b = sentimentArray
+	def average(self,numArray):
+		'''average sentiment'''
+		return sum(numArray)/len(numArray)
 
-		return sum(b)/len(b)
+	def trending(self):
+		'''trending sentiment'''
+
+		trendArray = []
+
+		for trend in Twitter().trends(cached=False):
+			trendArray.append([trend,indicoio.sentiment(trend)])
+
+		return trendArray
+
+
 		 
 def main():
-	sentiment = tweetSentiment('facebook', 10)
+	sentiment = tweetSentiment('wall street', 10)
 
-	sArr = sentiment.calculate()
+	#array of tweets and sentiment
+	fArr = sentiment.fArray()
+	#array of only tweet sentiment
+	sArr = sentiment.sArray()
+	#array of trending and sentiment
+	tArr = sentiment.trending()
 
-	print "Number of Tweets: " + str(len(sArr))
-	print "Subject Sentiment: "  + str(sentiment.average(sArr))
+	#print "Number of Tweets: " + str(len(sArr))
+	#print "Subject Sentiment: "  + str(sentiment.average(tArr))
 
 if __name__ == "__main__":
 	main()
